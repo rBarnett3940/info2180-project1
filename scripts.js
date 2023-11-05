0ed3446eead865343ca242de3c9b81fb3f601305
Richard <campion1boy@gmail.com>
2023-11-05 18:14:21 -0500

    Save 3

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* Add your JavaScript to this file */
+/*
+    let message = document.getElementsByClassName("message");
+
+    let btn = document.getElementsByClassName("btn")[1  ].onclick=function(e){
+
+        let email = document.getElementById("email").value;
+
+        if (email == ""){
+            message.textContent = 'Please enter a valid email address.';
+        }else{
+            message.textContent = 'Thank you! Your email address ${email} has been added to our mailing list!';
+        }
+
+      }*/
