107c1bfa187c5cdd6012c00cdc914aa8fd49a902
Richard <campion1boy@gmail.com>
2023-11-05 19:31:21 -0500

    Final Save

--- scripts.js
+++ scripts.js
@@ -1,15 +1,17 @@
 /* Add your JavaScript to this file */
-/*
-    let message = document.getElementsByClassName("message");
+var emailSubmit = function(){
+    var emailInput = document.getElementById('email');
+    var btn = document.getElementsByClassName('btn')[1].onclick=function(event){
+        var emailAddress = emailInput.value;
 
-    let btn = document.getElementsByClassName("btn")[1  ].onclick=function(e){
-
-        let email = document.getElementById("email").value;
-
-        if (email == ""){
-            message.textContent = 'Please enter a valid email address.';
-        }else{
-            message.textContent = 'Thank you! Your email address ${email} has been added to our mailing list!';
+        if (emailAddress == ''){
+            document.getElementsByClassName("message")[0].innerHTML= 'Please enter a valid email address.';
+        } else {
+            document.getElementsByClassName("message")[0].innerHTML= `Thank you! Your email address ${emailAddress} has been added to our mailing list!`;
         }
+        event.preventDefault();
+    }
+    btn.addEventListener("click",)
+}
 
-      }*/
+window.onload=emailSubmit
